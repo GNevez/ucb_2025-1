14ac36fb86bd37053516f74c5d98537c8419aee2
GUILHERME NEVES MONTANDON FERRAZ <guilherme.ferraz@a.ucb.br>
2025-02-28 10:49:37 -0300

    Atividade 1 Programacao web

--- atividade1/variaveisOperadores.js
+++ atividade1/variaveisOperadores.js
@@ -1,20 +1,38 @@
-console.log("Hello, world!");
-
 var x = 1;
-x = 2;
+var a = 3;
 
-console.log(x);
+var resultadoSoma = x + a;
+var resultadoSub = x - a;
+var resultadoMult = x * a;
+var resultadoDiv = x / a;
 
-let y = 1;
-y = 2;
+console.log(`Resultado da operação de soma entre ${x} e ${a} é ${resultadoSoma}`);
+console.log(`Resultado da operação de subtração entre ${x} e ${a} é ${resultadoSub}`);
+console.log(`Resultado da operação de multiplicação entre ${x} e ${a} é ${resultadoMult}`);
+console.log(`Resultado da operação de divisao entre ${x} e ${a} é ${resultadoDiv}`);
 
-console.log(y);
+let y = 8;
+let b = 4
 
-const z = 3;
+let resultadoSoma2 = y + b;
+let resultadoSub2 = y - b;
+let resultadoMult2 = y * b;
+let resultadoDiv2 = y / b;
 
-console.log(z);
+console.log(`Resultado da operação de soma entre ${y} e ${b} é ${resultadoSoma2}`);
+console.log(`Resultado da operação de subtração entre ${y} e ${b} é ${resultadoSub2}`);
+console.log(`Resultado da operação de multiplicação entre ${y} e ${b} é ${resultadoMult2}`);
+console.log(`Resultado da operação de divisao entre ${y} e ${b} é ${resultadoDiv2}`);
 
-let nome = "Guilherme Neves"
-let telefone = "61996360029"
+const z = 12;
+const c = 3;
 
-console.log("Meu nome é " + nome + ", numero: " + telefone);
+const resultadoSoma3 = z + c;
+const resultadoSub3 = y - b;
+const resultadoMult3 = y * b;
+const resultadoDiv3 = y / b;
+
+console.log(`Resultado da operação de soma entre ${z} e ${c} é ${resultadoSoma2}`);
+console.log(`Resultado da operação de subtração entre ${z} e ${c} é ${resultadoSub2}`);
+console.log(`Resultado da operação de multiplicação entre ${z} e ${c} é ${resultadoMult2}`);
+console.log(`Resultado da operação de divisao entre ${z} e ${c} é ${resultadoDiv2}`);
