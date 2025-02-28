5649d659734a3c0b3970c581c971b277d52c2dde
GUILHERME NEVES MONTANDON FERRAZ <guilherme.ferraz@a.ucb.br>
2025-02-28 10:37:40 -0300

    Atividade 1 Programacao web

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,20 @@
+console.log("Hello, world!");
+
+var x = 1;
+x = 2;
+
+console.log(x);
+
+let y = 1;
+y = 2;
+
+console.log(y);
+
+const z = 3;
+
+console.log(z);
+
+let nome = "Guilherme Neves"
+let telefone = "61996360029"
+
+console.log("Meu nome é " + nome + ", numero: " + telefone);
